b8551c3ddefc3129968e12aa0fecec6501c79a6a
reedtzdrvn <reedtzhere@gmail.com>
2024-02-19 15:09:20 +0300

    changed png icons to svg

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,43 +1,75 @@
 import { NavLink, useLocation } from "react-router-dom";
 
-import HomeIcon from '../../images/home.png'
-import FavoritesIcon from '../../images/favorite.png'
-import BasketIcon from '../../images/basket.png'
-import AccountIcon from '../../images/account.png'
+import HomeIcon from "../../images/home.svg";
+import FavoritesIcon from "../../images/favorites.svg";
+import BasketIcon from "../../images/basket.svg";
+import AccountIcon from "../../images/account.svg";
 
-import HomeIconActive from '../../images/home-active.png'
-import FavoritesIconActive from '../../images/favorite-active.png'
-import BasketIconActive from '../../images/basket-active.png'
-import AccountIconActive from '../../images/account-active.png'
+import HomeIconActive from "../../images/home-active.svg";
+import FavoritesIconActive from "../../images/favorites-active.svg";
+import BasketIconActive from "../../images/basket-active.svg";
+import AccountIconActive from "../../images/account-active.svg";
 
-import './footer.css'
+import { HandySvg } from "handy-svg";
+
+import "./footer.css";
 
 const Footer = (props) => {
-    const location = useLocation();
-    const currentPath = location.pathname;
-    console.log(currentPath)
-    console.log(location)
-
-    return (
-        <div className="flex justify-center">
-            <div className="footer w-[90%] fixed flex items-center justify-between">
-                <ul className="footer-nav flex justify-around w-full">
-                    <li>
-                        <NavLink to='/'><img src={currentPath === '/' ? HomeIconActive : HomeIcon} /></NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/favorites'><img src={currentPath === '/favorites' ? FavoritesIconActive : FavoritesIcon} /></NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/basket'><img src={currentPath === '/basket' ? BasketIconActive : BasketIcon} /></NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/account'><img src={currentPath === '/account' ? AccountIconActive : AccountIcon} /></NavLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  const location = useLocation();
+  const currentPath = location.pathname;
+  console.log(currentPath);
+  console.log(location);
+  // src={currentPath === '/' ? HomeIconActive : HomeIcon}
+  return (
+    <div className="flex justify-center">
+      <div className="footer w-[90%] fixed flex items-center justify-between">
+        <ul className="footer-nav flex justify-around w-full">
+          <li>
+            <NavLink to="/">
+              <HandySvg
+                src={currentPath === "/" ? HomeIconActive : HomeIcon}
+                width={30}
+                height={30}
+              />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/favorites">
+              <HandySvg
+                src={
+                  currentPath === "/favorites"
+                    ? FavoritesIconActive
+                    : FavoritesIcon
+                }
+                width={30}
+                height={30}
+              />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/basket">
+              <HandySvg
+                src={currentPath === "/basket" ? BasketIconActive : BasketIcon}
+                width={30}
+                height={30}
+              />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/account">
+              <HandySvg
+                src={
+                  currentPath === "/account" ? AccountIconActive : AccountIcon
+                }
+                width={30}
+                height={30}
+              />
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
-import Logo from '../../images/logo.png'
-import Search from '../../images/search.png'
-import On from '../../images/on.png'
-import Message from '../../images/message.png'
+import Logo from '../../images/logo.svg'
+import Search from '../../images/search.svg'
+import On from '../../images/on.svg'
+import Message from '../../images/message.svg'
 
 import './header.css'
+import { HandySvg } from 'handy-svg'
 
 const Header = () => {
     return (
         <div className='flex justify-center align-center'>
             <div className="header w-[90%] h-[5%] flex items-center justify-between">
                 <div className='header-left-side flex justify-start align-center'>
-                    <img className='header-logo flex ml-[5px]' src={Logo} />
+                    <HandySvg className='header-logo flex ml-[5px]' src={Logo} />
                     <span className='company_name w-[30px] h-[50px] text-[10px] leading-[10px] flex items-center font-bold'>
                         World
                         Transport
@@ -19,13 +20,13 @@ const Header = () => {
                 </div>
                 <div className='header-right-side flex justify-end items-center align-center'>
                     <div className='search-btn'>
-                        <img className='search-btn' src={Search} />
+                        <HandySvg className='search-btn' src={Search} />
                     </div>
                     <div className='notifications-btn'>
-                        <img src={On} />
+                        <HandySvg src={On} />
                     </div>
                     <div className='message-btn'>
-                        <img className='message-btn' src={Message} />
+                        <HandySvg style={{fill: 'black'}} className='message-btn' width={20} height={20} src={Message} />
                     </div>
                 </div>
             </div>
