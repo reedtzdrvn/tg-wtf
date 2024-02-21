e7f9f55feea72cabfadbfd455152f39a9fbbcb38
Knexy04 <nikitos.markin04@gmail.com>
2024-02-22 01:19:20 +0300

    [Frontend] - fix bad input

--- frontend/src/components/EspeciallyForYouPage/EspeciallyForYouPage.jsx
+++ frontend/src/components/EspeciallyForYouPage/EspeciallyForYouPage.jsx
@@ -6,7 +6,7 @@ const EspeciallyForYouPage = () => {
     <div className="page">
       <div className="mx-[8.5%] mt-[32px] relative">
         <img src={background} alt="1" className="imgbanner" />
-        <div className="absolute top-0 bottom-0 w-full text-white px-[10%] py-[12px]">
+        <div className="absolute top-0 bottom-0 w-full text-white px-[10%] py-[12px] h-max">
           <div className="lend flex flex-col gap-2 leading-10 font-bold">
             <div className="text-left">ESPECIALLY</div>
             <div className="text-right">FOR YOU</div>
