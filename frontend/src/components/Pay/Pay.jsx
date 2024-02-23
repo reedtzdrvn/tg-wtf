edfbcf464088d91b70762b4c2b53c49226eed43b
Daniil Markov <reedtzhere@gmail.com>
2024-02-23 21:43:06 +0300

    [Frontend] - fixed Pay.jsx page

--- frontend/src/components/Pay/Pay.jsx
+++ frontend/src/components/Pay/Pay.jsx
@@ -1,6 +1,15 @@
 import "./Pay.css";
 import PaymentRow from "../PaymentRow/PaymentRow"
 
+import mir from "../../images/mir.svg"
+import visa from "../../images/Visa.svg"
+import mastercard from "../../images/Mastercard.svg"
+import amex from '../../images/Amex.svg'
+import unionPay from '../../images/UnionPay.svg'
+import webmoney from '../../images/Webmoney.svg'
+import Bitcoin from '../../images/Bitcoin.svg'
+import Etherium from '../../images/Etherium.svg'
+import USDT from '../../images/USDT.svg'
 
 const Pay = () => {
     return (
@@ -15,9 +24,9 @@ const Pay = () => {
             </div>
             <div className="textChoose flex justify-center ml-[6%] mr-[24%]">Choose a suitable payment <br/> method</div>
             <div className="paymentsline1 flex justify-center mx-[17%]"></div>
-            <PaymentRow />
-            <PaymentRow />
-            <PaymentRow />
+            <PaymentRow imgSrc1={mir} imgSrc2={visa} imgSrc3={mastercard} />
+            <PaymentRow imgSrc1={amex} imgSrc2={unionPay} imgSrc3={webmoney} />
+            <PaymentRow imgSrc1={Bitcoin} imgSrc2={Etherium} imgSrc3={USDT} />
         </div>
     );
 };
