f4476fe8a83dd2bbe90442668aef6e80fd7a5f35
Daniil Markov <reedtzhere@gmail.com>
2024-02-23 23:17:26 +0300

    fixed package.json & package-lock.json

--- frontend/src/components/Pay/Pay.jsx
+++ frontend/src/components/Pay/Pay.jsx
@@ -1,35 +1,36 @@
 import "./Pay.css";
-import PaymentRow from "../PaymentRow/PaymentRow"
+import PaymentRow from "../PaymentRow/PaymentRow";
 
-import mir from "../../images/mir.svg"
-import visa from "../../images/Visa.svg"
-import mastercard from "../../images/Mastercard.svg"
-import amex from '../../images/Amex.svg'
-import unionPay from '../../images/UnionPay.svg'
-import webmoney from '../../images/Webmoney.svg'
-import Bitcoin from '../../images/Bitcoin.svg'
-import Etherium from '../../images/Etherium.svg'
-import USDT from '../../images/USDT.svg'
+import mir from "../../images/mir.svg";
+import visa from "../../images/Visa.svg";
+import mastercard from "../../images/Mastercard.svg";
+import amex from "../../images/Amex.svg";
+import unionPay from "../../images/UnionPay.svg";
+import webmoney from "../../images/Webmoney.svg";
+import Bitcoin from "../../images/Bitcoin.svg";
+import Etherium from "../../images/Etherium.svg";
+import USDT from "../../images/USDT.svg";
 
 const Pay = () => {
-    return (
-        <div className="page paymentwrapper">
-            <div className="bluerectangle ml-[63px] mt-[80px] relative flex flex-col">
-                <div className="ml-[43px]">
-                    <div className="textbalance">Total Balance</div>
-                    <div className="textsumbasket">$24,000</div>
-                    <div className="orangerectangle"></div>
-                    <div className="purplerectangle"></div>
-                </div>
-            </div>
-            <div className="textChoose flex justify-center ml-[6%] mr-[24%]">Choose a suitable payment <br/> method</div>
-            <div className="paymentsline1 flex justify-center mx-[17%]"></div>
-            <PaymentRow imgSrc1={mir} imgSrc2={visa} imgSrc3={mastercard} />
-            <PaymentRow imgSrc1={amex} imgSrc2={unionPay} imgSrc3={webmoney} />
-            <PaymentRow imgSrc1={Bitcoin} imgSrc2={Etherium} imgSrc3={USDT} />
+  return (
+    <div className="page paymentwrapper">
+      <div className="bluerectangle ml-[63px] mt-[80px] relative flex flex-col">
+        <div className="ml-[43px]">
+          <div className="textbalance">Total Balance</div>
+          <div className="textsumbasket">$24,000</div>
+          <div className="orangerectangle"></div>
+          <div className="purplerectangle"></div>
         </div>
-    );
+      </div>
+      <div className="textChoose flex justify-center ml-[6%] mr-[24%]">
+        Choose a suitable payment <br /> method
+      </div>
+      <div className="paymentsline1 flex justify-center mx-[17%]"></div>
+      <PaymentRow imgSrc1={mir} imgSrc2={visa} imgSrc3={mastercard} />
+      <PaymentRow imgSrc1={amex} imgSrc2={unionPay} imgSrc3={webmoney} />
+      <PaymentRow imgSrc1={Bitcoin} imgSrc2={Etherium} imgSrc3={USDT} />
+    </div>
+  );
 };
-
 
 export default Pay;
--- frontend/src/components/PaymentRow/PaymentRow.jsx
+++ frontend/src/components/PaymentRow/PaymentRow.jsx
@@ -1,20 +1,24 @@
 import "./PaymentRow.css";
 
-
-const PaymentRow = ({imgSrc1, imgSrc2, imgSrc3}) => {
-    return (
-        <div className ="flex justify-center">
-            <div className="paymentmethodsrectangle">
-                <div className="grid grid-cols-3 mx-[45px]">
-                    <div className = "paymentmethods mx-[1px] flex justify-center" ><img src={imgSrc1} alt="" /></div>
-                    <div className = "paymentmethods mx-[1px] flex justify-center" ><img src={imgSrc2} alt="" /></div>
-                    <div className = "paymentmethods mx-[1px] flex justify-center" ><img src={imgSrc3} alt="" /></div>   
-                </div>
-                <div className="paymentslines flex justify-center mx-[11%]"></div>
-            </div>
+const PaymentRow = ({ imgSrc1, imgSrc2, imgSrc3 }) => {
+  return (
+    <div className="flex justify-center">
+      <div className="paymentmethodsrectangle">
+        <div className="grid grid-cols-3 mx-[45px]">
+          <div className="paymentmethods mx-[1px] flex justify-center">
+            <img src={imgSrc1} alt="" />
+          </div>
+          <div className="paymentmethods mx-[1px] flex justify-center">
+            <img src={imgSrc2} alt="" />
+          </div>
+          <div className="paymentmethods mx-[1px] flex justify-center">
+            <img src={imgSrc3} alt="" />
+          </div>
         </div>
-        
-    );
+        <div className="paymentslines flex justify-center mx-[11%]"></div>
+      </div>
+    </div>
+  );
 };
 
 export default PaymentRow;
