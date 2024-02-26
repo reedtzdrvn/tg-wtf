df1193aee96667bcd17bace9dffce7e1cb36aa0f
Knexy04 <nikitos.markin04@gmail.com>
2024-02-26 23:05:22 +0300

    [Backend] - init

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 4444
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import {config as dotenvConfig} from 'dotenv';
+
+dotenvConfig();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.NODE_DB_URL)
+    .then(() => console.log('DB OK'))
+    .catch((err) => console.log('DB error', err));
+
+const PORT = process.env.PORT || 4444
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Server is running');
+});
+
 
 app.get('/', (req, res) => {
   console.log('priv')
   res.send('hi')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
 })
