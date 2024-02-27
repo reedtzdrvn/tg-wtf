ef0c1baade0d4abf2a87751e2493ddcc9e8bf43c
Daniil Markov <reedtzhere@gmail.com>
2024-02-27 03:00:33 +0300

    [Bot] zalupa

--- bot/tg-bot/main.py
+++ bot/tg-bot/main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from aiogram import Bot, Dispatcher, start_webhook
+from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
@@ -9,25 +9,25 @@ from aiogram.fsm.storage.memory import MemoryStorage
 import config
 from handlers import router
 
-WEBHOOK_HOST = 'https://wtf.alwaysdata.net/'
-WEBHOOK_PATH = '/bot/'
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+# WEBHOOK_HOST = 'https://wtf.alwaysdata.net/'
+# WEBHOOK_PATH = '/bot/'
+# WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
-WEBAPP_HOST = '::'
-WEBAPP_PORT = 8399
+# WEBAPP_HOST = '::'
+# WEBAPP_PORT = 8399
 
 async def main():
     bot = Bot(token=config.BOT_TOKEN, parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path=WEBHOOK_PATH,
-        skip_updates=True,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
-    await bot.delete_webhook(drop_pending_updates=True)
+    # start_webhook(
+    #     dispatcher=dp,
+    #     webhook_path=WEBHOOK_PATH,
+    #     skip_updates=True,
+    #     host=WEBAPP_HOST,
+    #     port=WEBAPP_PORT,
+    # )
+    # await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 
