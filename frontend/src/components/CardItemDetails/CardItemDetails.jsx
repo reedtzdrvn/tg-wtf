82cb5d9fc5bc57f477b7211e2664bdc007eec06a
Daniil Markov <reedtzhere@gmail.com>
2024-02-29 17:13:15 +0300

    ngrok

--- frontend/src/components/CardItemDetails/CardItemDetails.jsx
+++ frontend/src/components/CardItemDetails/CardItemDetails.jsx
@@ -13,6 +13,7 @@ import CardItemDetailsBasketButton from "./CardItemDetailsBasketButton";
 
 import CardItemStars from "./CardItemStars";
 import CardItemDetailsRatings from "./CardItemDetailsRatings";
+import Preloader from "../errors/Preloader.js";
 
 const CardItemDetails = (props) => {
   const [item, setItem] = useState(false);
@@ -36,6 +37,9 @@ const CardItemDetails = (props) => {
       
   }, []);
 
+  if (!item) {
+    return <Preloader />
+  }
 
   const changeShowRatingsHandler = () => {
     setShowRatings(!showRatings);
@@ -63,21 +67,7 @@ const CardItemDetails = (props) => {
       if (currentAmount === 0) setCurrentAmount(1);
     }
   };
-  console.log(item)
-  // const item = {
-  //   title: "Cappadacia",
-  //   ratingsCount: 258,
-  //   learnersCount: 1983,
-  //   price: 2400,
-  //   estimateDeliveryTime: "5-7",
-  //   availableSizes: ["XXS", "S", "M", "L", "XL", "XXL"],
-  //   description:
-  //     "Cappadocia hot air balloon can be pricey but it all comes down to what you may want to be included in the price. The price to ride a hot air balloon is in the range between $140 and $250 (€125 – €220) per person. The cost depends on the duration of the flight, the moment of the day and whether is peak season or not",
-  //   reviewsCount: 653,
-  //   ratingsScore: 4.5,
-  //   images: [slide1, slide2, slide3],
-  //   category: "Hockey uniform",
-  // };
+
 
   return (
     <>
